fd5adb6515701d7a4fbb912217099648d9bafba6
Alexey Kim <kmlx@yandex.ru>
2021-02-22 18:54:59 +0300

    Change

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 from brain_games import cli
 
@@ -23,46 +22,5 @@ def play():
         if cli.check_result(int(answer), result):
             continue
         else:
-            return True
-    return True
-||||||| parent of 27d83dd (Change)
-=======
-import random
-from brain_games import cli
-
-
-def play():
-    print('Find the greatest common divisor of given numbers.')
-    for _ in range(cli.GAME_ROUNDS):
-        k = random.randint(2, cli.VALUE_INTERVAL)
-        m = random.randint(2, cli.VALUE_INTERVAL)
-        number1 = random.randint(2, cli.VALUE_INTERVAL)
-        number2 = number1 * k
-        number1 = number1 * m
-        result = 1
-        for i in range(number1, 1, -1):
-            if number1 % i or number2 % i:
-                continue
-            else:
-                result = i
-                break
-        q_set = [str(number1), str(number2)]
-        answer = cli.ask_answer(' '.join(q_set))
-        if cli.check_result(int(answer), result):
-            continue
-        else:
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
             return False
-=======
-            return True
->>>>>>> d40d6ce0b1faefa156e967faca3c1bbc2558a91c
-=======
-            return True
->>>>>>> d40d6ce0b1faefa156e967faca3c1bbc2558a91c
-=======
-            return True
->>>>>>> d40d6ce0b1faefa156e967faca3c1bbc2558a91c
     return True
->>>>>>> 27d83dd (Change)
